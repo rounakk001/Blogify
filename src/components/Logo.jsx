3d38209008f9922d4028820e9bceba9542d23662
rounakk001 <rounaksingh2303@gmail.com>
2026-06-01 23:25:00 +0530

    fixed logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 function Logo() {
   return (
-    <div className="font-extrabold text-3xl tracking-wide">
-      Blog<span className="text-blue-500">Hub</span>
+    <div className="group cursor-pointer text-3xl font-black tracking-tight">
+      <span className="text-black">Blog</span>
+      <span className="bg-gradient-to-r from-blue-400 via-cyan-300 to-blue-500 bg-[length:200%_auto] bg-clip-text text-transparent transition-all duration-700 group-hover:bg-right">
+        ify
+      </span>
     </div>
   )
 }
-
-
 
 export default Logo
